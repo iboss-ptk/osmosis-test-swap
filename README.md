327bf4edea1dedd16f53128b9e784989ac70a9ee
user-8295400 <>
2023-09-07 12:02:52 +0000

    Preview

---
+++ README.md
@@ -0,0 +1,8 @@
+## Osmosis Swap Test
+This is a simple contract which just swaps received OSMO tokens through Osmosis DEX using OSMO/ION pool on testnet.
+
+## Build
+Only for testing purposes
+```bash
+cargo cw-optimizoor
+```
---
+++ contracts/swap_test/src/lib.rs
@@ -0,0 +1,64 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{
+    entry_point, Coin, CosmosMsg, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+};
+use cw_utils::one_coin;
+use osmosis_std::types::osmosis::poolmanager::v1beta1::{
+    MsgSwapExactAmountIn, MsgSwapExactAmountOut, SwapAmountInRoute, SwapAmountOutRoute,
+};
+
+#[entry_point]
+fn instantiate(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[cw_serde]
+enum ExecuteMsg {
+    Direct {},
+    Reverse { want: Coin, token_in_denom: String },
+}
+
+#[entry_point]
+fn execute(_deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
+    let swap_msg: CosmosMsg = match msg {
+        ExecuteMsg::Direct {} => {
+            let offer_coin = one_coin(&info).unwrap();
+            let token_out_denom = match offer_coin.denom.as_str() {
+                "factory/osmo1cyyzpxplxdzkeea7kwsydadg87357qnahakaks/fakeusdt" => {
+                    "factory/osmo1cyyzpxplxdzkeea7kwsydadg87357qnahakaks/fakeusdc"
+                }
+                "factory/osmo1cyyzpxplxdzkeea7kwsydadg87357qnahakaks/fakeusdc" => {
+                    "factory/osmo1cyyzpxplxdzkeea7kwsydadg87357qnahakaks/fakeusdt"
+                }
+                _ => panic!("Unsupported coin"),
+            }
+            .to_string();
+
+            MsgSwapExactAmountIn {
+                sender: env.contract.address.to_string(),
+                routes: vec![SwapAmountInRoute {
+                    pool_id: 86,
+                    token_out_denom,
+                }],
+                token_in: Some(offer_coin.into()),
+                token_out_min_amount: "1".to_string(),
+            }
+            .into()
+        }
+        ExecuteMsg::Reverse {
+            want,
+            token_in_denom,
+        } => MsgSwapExactAmountOut {
+            sender: info.sender.to_string(),
+            routes: vec![SwapAmountOutRoute {
+                pool_id: 86,
+                token_in_denom,
+            }],
+            token_in_max_amount: u128::MAX.to_string(),
+            token_out: Some(want.into()),
+        }
+        .into(),
+    };
+
+    Ok(Response::new().add_message(swap_msg))
+}
